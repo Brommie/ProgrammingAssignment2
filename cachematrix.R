c0f8d4a80e9d42bb6170ed13fee9ea66839b3110
Erin Janelle <erinjanelleshort@Erins-MacBook-Pro.local>
2014-06-21 21:32:33 -0700

    add first function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
